580192ed2847dada3b651aae1e2a973472c0323d
yuu-chien <sarsh40320@gmail.com>
2024-01-18 15:34:26 +0800

    chore: remove files to common folder

---
+++ src/components/common/Headline.tsx
@@ -0,0 +1,31 @@
+import { Box, Stack, Typography } from '@mui/material';
+
+interface HeadlineProps {
+  title: string;
+}
+
+function Headline(props: HeadlineProps) {
+  const { title } = props;
+  return (
+    <Box display="flex" alignItems="center">
+      <Stack
+        sx={{
+          width: '4px',
+          height: '24px',
+          mr: '12px',
+          bgcolor: 'primary.main',
+          borderRadius: '10px',
+        }}
+      />
+      <Typography
+        sx={{
+          fontSize: { sm: '24px' },
+          fontWeight: 700,
+        }}>
+        {title}
+      </Typography>
+    </Box>
+  );
+}
+
+export default Headline;
