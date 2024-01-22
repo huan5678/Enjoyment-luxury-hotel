423bd85c7339f410a846abb8de52e8326a60f39f
yuu-chien <sarsh40320@gmail.com>
2024-01-22 18:29:30 +0800

    [wip]: lay out the page using fake data

--- src/app/(room)/room-type/page.tsx
+++ src/app/(room)/room-type/page.tsx
@@ -1,29 +1,31 @@
 'use client';
 
-import { Box, Container, Grid, Typography } from '@mui/material';
+import { Box, Grid, Stack, Typography } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import RoomTypeCard from '@/components/room/RoomTypeCard';
+import roomTypeFakeData from './fakeData';
 
 export default function Page() {
   const theme = useTheme();
   const matches = useMediaQuery(() => theme.breakpoints.down('md'));
   return (
-    <Container maxWidth="xl" sx={{ backgroundColor: '#f7f2ee' }}>
-      <Typography variant={matches ? 'body2' : 'h6'}>房型選擇</Typography>
-      <Typography variant={matches ? 'h3' : 'h1'} color="primary.main" mt={matches ? 1 : 2}>
-        各種房型，任您挑選
-      </Typography>
-      <Box component="section">
-        <Grid container spacing={matches ? 3 : 6} width="100%">
-          <Grid item sm={12}>
-            <RoomTypeCard></RoomTypeCard>
+    <Box width="100%" px={matches ? '12px' : '60px'} sx={{ backgroundColor: '#f7f2ee' }}>
+      <Stack py={matches ? 5 : '120px'}>
+        <Typography variant={matches ? 'body2' : 'h6'}>房型選擇</Typography>
+        <Typography variant={matches ? 'h3' : 'h1'} color="primary.main" mt={matches ? 1 : 2}>
+          各種房型，任您挑選
+        </Typography>
+        <Box component="section" mt={matches ? 5 : 10}>
+          <Grid container spacing={matches ? 3 : 6}>
+            {roomTypeFakeData.map((room) => (
+              <Grid item sm={12} key={room._id} width="100%">
+                <RoomTypeCard {...room}></RoomTypeCard>
+              </Grid>
+            ))}
           </Grid>
-          <Grid item sm={12}>
-            <RoomTypeCard></RoomTypeCard>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+        </Box>
+      </Stack>
+    </Box>
   );
 }
