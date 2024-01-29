886e4ee3fa6c0c70091be958bc9a61c1adbcefea
yuu-chien <sarsh40320@gmail.com>
2024-01-29 22:38:34 +0800

    fix: use container

--- src/app/(room)/room-type/page.tsx
+++ src/app/(room)/room-type/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Grid, Stack, Typography } from '@mui/material';
+import { Box, Container, Grid, Stack, Typography } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import RoomTypeCard from '@/app/(room)/room-type/RoomTypeCard';
@@ -10,22 +10,24 @@ export default function Page() {
   const theme = useTheme();
   const matches = useMediaQuery(() => theme.breakpoints.down('md'));
   return (
-    <Box width="100%" px={matches ? '12px' : '60px'} sx={{ backgroundColor: '#f7f2ee' }}>
-      <Stack py={matches ? 5 : '120px'}>
-        <Typography variant={matches ? 'body2' : 'h6'}>房型選擇</Typography>
-        <Typography variant={matches ? 'h3' : 'h1'} color="primary.main" mt={matches ? 1 : 2}>
-          各種房型，任您挑選
-        </Typography>
-        <Box component="section" mt={matches ? 5 : 10}>
-          <Grid container spacing={matches ? 3 : 6}>
-            {roomTypeFakeData.map((room) => (
-              <Grid item sm={12} key={room._id} width="100%">
-                <RoomTypeCard {...room}></RoomTypeCard>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-      </Stack>
+    <Box width="100%" sx={{ backgroundColor: '#f7f2ee' }}>
+      <Container>
+        <Stack py={matches ? 5 : '120px'}>
+          <Typography variant={matches ? 'body2' : 'h6'}>房型選擇</Typography>
+          <Typography variant={matches ? 'h3' : 'h1'} color="primary.main" mt={matches ? 1 : 2}>
+            各種房型，任您挑選
+          </Typography>
+          <Box component="section" mt={matches ? 5 : 10}>
+            <Grid container spacing={matches ? 3 : 6}>
+              {roomTypeFakeData.map((room) => (
+                <Grid item sm={12} key={room._id} width="100%">
+                  <RoomTypeCard {...room}></RoomTypeCard>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Stack>
+      </Container>
     </Box>
   );
 }
